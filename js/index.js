1ad6b997c7184df7c7434da0f926f53a9abb6f24
Adebayo Temitope <adecancode@gmail.com>
2020-07-17 00:55:01 +0100

    Major Updates

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+    $(function() {
+        $('form').submit(function(e) {
+      
+          //prevent default form submission
+          e.preventDefault();
+      
+         //ajax form submission
+            $.ajax({
+                type: 'POST',
+                url: 'subscribe.php',
+                data: $(this).serialize(),
+                success: (message) => {
+                 // add an action to be carried out when submission is successful
+                  alert(`${message}`)
+                },
+                // add an action to be carried out when submission is successful
+                error: (error) => {
+                  alert(`${error}`)
+                }
+                
+            })
+        }); 
+      })
+      $('.pop').magnificPopup({
+        type: 'image'
+        // other options
+      });
+      $(document).ready(function() {
+
+        $('.image-popup-vertical-fit').magnificPopup({
+          type: 'image',
+          closeOnContentClick: true,
+          mainClass: 'mfp-img-mobile',
+          image: {
+            verticalFit: true
+          }
+          
+        });
+      
+      });
+   
+   
+    
---
+++ subscribe.php
@@ -0,0 +1,29 @@
+<?php
+
+// get value of email on submit
+  $email = $_POST['email'];
+
+  $conn = new mysqli('localhost','root','','earlyaccess');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into subscribers(email) values(?)");
+		$stmt->bind_param("s", $email);
+		$execval = $stmt->execute();
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+
+	if (empty($_POST["email"])) {
+		$emailErr = "Email is required";
+	  } else {
+		$email = ($_POST["email"]);
+		// check if e-mail address is well-formed
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  $emailErr = "Invalid email format";
+		}
+	  }
+
+?>
